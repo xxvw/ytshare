3bfe78adae7d0543383b37970493f603a9b36448
y <92621736+xxvw@users.noreply.github.com>
2023-05-29 00:13:09 +0900

    Add files via upload

---
+++ src/app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import flask
+import json
+
+from os.path import dirname, abspath
+import sys
+sys.path.extend([dirname(dirname(dirname(abspath(__file__)))), dirname(dirname(abspath(__file__)))])
+
+print('flask version', flask.__version__)
+app = flask.Flask(__name__, template_folder='templates')
+app.secret_key = b'secret_key_sample'
+
+# modules-routes
+from src.routes.indexRoute import index_root
+
+app.register_blueprint(index_root)
+
+# print all routes
+print('routes:')
+for rule in app.url_map.iter_rules():
+    print(rule)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
---
+++ src/routes/indexRoute.py
@@ -0,0 +1,34 @@
+import flask
+from flask import Blueprint
+import json
+
+index_root = Blueprint('index', __name__)
+
+@index_root.route('/', methods=['GET'])
+def index():
+    if flask.request.method == 'GET':
+        # lobby_name, user_name, created_at, participants, id
+        testlobbies = [
+            {
+                'lobby_name': 'lobby1',
+                'user_name': 'user1',
+                'created_at': '2020-01-01 00:00:00',
+                'participants': 1,
+                'id': 1
+            },
+            {
+                'lobby_name': 'lobby2',
+                'user_name': 'user2',
+                'created_at': '2020-01-01 00:00:00',
+                'participants': 2,
+                'id': 2
+            },
+            {
+                'lobby_name': 'lobby3',
+                'user_name': 'user3',
+                'created_at': '2020-01-01 00:00:00',
+                'participants': 3,
+                'id': 3
+            }
+        ]
+        return flask.render_template('index.html', public_lobbies=testlobbies)
